67f44c25621376fa77ebcaf1aeab2d6951a7ddcc
krisharathod1009 <98577504+krisharathod1009@users.noreply.github.com>
2024-11-30 22:35:53 +0530

    Delete frontend/GrowChain-Frontend directory

--- frontend/GrowChain-Frontend/lib/isLoggedIn.js
+++
@@ -1,9 +0,0 @@
-const { cookies } = require("next/headers");
-const { redirect } = require("next/navigation");
-
-export const isLoggedIn = () => {
-  const address = cookies().get("address")?.value;
-  if (address) {
-    redirect("/feed");
-  }
-};
